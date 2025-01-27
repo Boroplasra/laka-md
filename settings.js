bda88829ff00c7b61d7c035de00f7813ed66b921
Boroplasra <boropleas@gmail.com>
2025-01-27 21:43:47 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,11 +7,11 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 //👉 Put Your Session Id 🐣 👈කියන එක කපලා අයින් කරලා ඔයාගේ Session id එක දාන්න.
-SESSION_ID: process.env.SESSION_ID || "Put Your Session Id 🐣",
+SESSION_ID: process.env.SESSION_ID || "LAKA-MD=M54C0RzI#vu23acCuRfZ4ZMzCeOTFDLjf5Vn4bn0q1BqYCEoR_4w",
 //👉 Put Your Mongode Url 🖇️ 👈කියන එක කපලා අයින් කරලා ඔයාගේ Mongode Url එක දාන්න.
-MONGODB: process.env.MONGODB || "Put Your Mongode Url 🖇️",
+MONGODB: process.env.MONGODB || "mongodb://mongo:GdCfQflUXajCbEhqTEFNBOJIOACcuBSl@junction.proxy.rlwy.net:23221",
 PREFIX: process.env.PREFIX || '.' ,
-OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94781121579' : process.env.OWNER_NUMBER,
+OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '919387196256' : process.env.OWNER_NUMBER,
 OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39",
 DELETEMSGSENDTO : process.env.DELETEMSGSENDTO === undefined ? '' : process.env.DELETEMSGSENDTO
 };
